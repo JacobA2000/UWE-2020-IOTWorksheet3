4083ab33d33e475fece771f722b1b5e61c61c58e
j58-allen <jacob2.allen@live.uwe.ac.uk>
2021-04-18 16:15:19 +0100

    Task 1 Code Cleanup

--- Task 1/main.py
+++ Task 1/main.py
@@ -2,34 +2,32 @@ import asyncio
 import websockets
 import base64
 
-async def recv_packet(websocket):
-    """Recvieves the udp packet."""
+async def recv_packet(websocket) -> bytes:
+    """Recieves the udp packet."""
     packet = await websocket.recv()
 
     print(f"Base64: {packet}")
-    return  base64.b64decode(packet)
+    return base64.b64decode(packet)
 
-async def decode_packet(websocket):
+async def decode_packet(websocket) -> None:
     """Decodes the recvied packet"""
     packet = await recv_packet(websocket)
-    print(f"UDP: {packet}")   
-
     source_port = int.from_bytes(packet[0:2], "little")
-    print(f"Source Port: {source_port}")
-
     dest_port = int.from_bytes(packet[2:4], "little")
-    print(f"Dest Port: {dest_port}")
-
     length = int.from_bytes(packet[4:6], "little")
-    print(f"Length: {length}")
-
     checksum = int.from_bytes(packet[6:8], "little")
-    print(f"Checksum: {checksum}")
-
     payload = packet[8:(length+8)].decode("utf-8")
-    print(f"Payload: {payload}")
 
-async def main():
+    print("-" * (24 + len(str(packet))))
+    print(f"UDP:                    {packet}")
+    print(f"Source Port:            {source_port}")
+    print(f"Dest Port:              {dest_port}")
+    print(f"Length:                 {length}")
+    print(f"Checksum:               {checksum}")
+    print(f"Payload:                {payload}")
+    print("-" * (24 + len(str(packet))))
+
+async def main() -> None:
     uri = "ws://localhost:5612"
 
     async with websockets.connect(uri) as websocket:
